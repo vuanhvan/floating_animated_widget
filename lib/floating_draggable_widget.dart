605760a13edce85e60ae8d80eb40e6dbffda27cd
vuanhvan <vuanhvan@hotmail.com>
2024-11-25 17:24:53 +0700

    Update floating_draggable_widget.dart

--- lib/floating_draggable_widget.dart
+++ lib/floating_draggable_widget.dart
@@ -122,7 +122,7 @@ class _FloatingDraggableWidgetState extends State<FloatingDraggableWidget>
   /// distance from top and left initial value
   late double top, left;
   double? right = 20;
-  double? bottom = 20;
+  double? bottom = 0;
 
   /// is the widget tabbed bool value
   bool isTabbed = false;
@@ -262,7 +262,7 @@ class _FloatingDraggableWidgetState extends State<FloatingDraggableWidget>
                           : null,
                       bottom: widget.bottom ??
                           (widget.dy == null && left == -1 && top == -1
-                              ? 20
+                              ? 0
                               : null),
                       duration: Duration(milliseconds: isDragging ? 100 : 700),
 
