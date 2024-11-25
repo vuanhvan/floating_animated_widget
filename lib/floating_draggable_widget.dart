2339711a5201929ef9d8db9c1744f68eb29063e2
vuanhvan <vuanhvan@hotmail.com>
2024-11-25 17:13:02 +0700

    Update floating_draggable_widget.dart

--- lib/floating_draggable_widget.dart
+++ lib/floating_draggable_widget.dart
@@ -417,8 +417,8 @@ class _FloatingDraggableWidgetState extends State<FloatingDraggableWidget>
     /// top variable will be no more than the screen total height
     double currentTop;
     // 60 is the height of the appbar
-    if (dy >= (totalHeight - widget.floatingWidgetHeight) - 100) {
-      currentTop = (totalHeight - widget.floatingWidgetHeight) - 100;
+    if (dy >= (totalHeight - widget.floatingWidgetHeight) - 60) {
+      currentTop = (totalHeight - widget.floatingWidgetHeight) - 60;
     } else {
       if (dy <= 0) {
         currentTop = widget.floatingWidgetHeight;
